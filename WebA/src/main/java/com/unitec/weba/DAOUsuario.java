4c4516f369742bb8adac4b06b620fdcfeeac39cc
gatuzo07 <rmendozar@live.com.mx>
2015-06-09 21:47:12 -0500

    Creación DAOUsuario y sus metodos @daton

---
+++ WebA/src/main/java/com/unitec/weba/DAOUsuario.java
@@ -0,0 +1,61 @@
+package com.unitec.weba;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author gatuzo07
+ *
+ * Inicializar la sesión
+ * throws Exception:  Para lanzar excepciones por si surge algun error
+ * Se manejan bloques estaticos y metodos estaticos
+ */
+public class DAOUsuario {
+    static SessionFactory factory;
+    static Session sesion;
+    static Transaction tranza; 
+    static{
+        factory= HibernateUtilidades.getSessionFactory();
+        sesion= factory.openSession();
+        tranza= sesion.beginTransaction();
+    }
+    
+    public static void cerrarTodo(){
+        tranza.commit();
+        sesion.close();
+    }
+    
+    public DAOUsuario(){
+        
+    }
+//Creamos el insert
+    public void guardar(Usuario u)throws Exception{
+//Aqui poner el codigo de tu guardado
+//Despues invocar el metodo cerrarTodo
+        sesion.save(u);
+        cerrarTodo();
+    }
+//Creamos el update
+    public void actualizar(Usuario u)throws Exception{
+         
+        cerrarTodo();
+    }
+//Creamos el buscarTodos
+    public List<Usuario> buscarTodos()throws Exception{
+        
+        return null;
+    }
+//Creamos el buscar por Id
+    public Usuario buscarPorId(Integer id)throws Exception{
+        
+        return null;
+    }
+//Creamos el de autenticar
+    public boolean autenticar(Usuario u)throws Exception{
+        
+        return false;
+    }
+}
