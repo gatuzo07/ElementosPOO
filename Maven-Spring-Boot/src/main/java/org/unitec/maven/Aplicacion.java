b6b706e6b2970a9ccfbc8612866c5daa6db6fa06
gatuzo07 <rmendozar@live.com.mx>
2015-07-10 21:49:04 -0500

    LeerArchivo Spring Boot @daton

--- Maven-Spring-Boot/src/main/java/org/unitec/maven/Aplicacion.java
+++ Maven-Spring-Boot/src/main/java/org/unitec/maven/Aplicacion.java
@@ -9,15 +9,22 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
  * Esta sera la aplicación principal, usaremos la tecnologia de Spring "anotación"
  * la que autoconfigurara la visualización ya sea web o desktop y la configuración
  * de tommcat.
- * @EnableAutoConfiguration - Configura todo el tommcat
- * 
+ * @EnableAutoConfiguration - Indica al contenerdor de Spring configurar todo:
+ *                          -localhost
+ *                          -Tommcat
+ *                          -Contexto de Spring (http://localhost:8080/)
+ *                          -Otras paginas
  */
 
 @EnableAutoConfiguration
 public class Aplicacion {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         SpringApplication.run(Aplicacion.class, args);
         
+        System.out.println("Un mensaje desde spring boot");
+        
+        LeerArchivo archivo=new LeerArchivo();
+        System.out.println(archivo.leer());        
     }
 }
---
+++ Maven-Spring-Boot/src/main/java/org/unitec/maven/LeerArchivo.java
@@ -0,0 +1,22 @@
+package org.unitec.maven;
+
+/**
+ * @author gatuzo07
+ */
+
+import java.io.*;
+
+public class LeerArchivo {
+    
+    public String leer() throws Exception{
+        File file=new File("/Users/gatuzo07/Desktop/Lectura.txt");
+        FileInputStream fis=new FileInputStream(file);
+        StringBuilder sb=new StringBuilder();
+        int ch=0;
+        while((ch=fis.read())!=-1){
+            sb.append((char)ch);
+        }
+        return sb.toString();
+    }
+    
+}
