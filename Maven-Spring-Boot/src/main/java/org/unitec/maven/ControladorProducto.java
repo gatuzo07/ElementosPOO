cc359f0388a45b9d887a4b7246faa0c9de52fdf0
gatuzo07 <gatuzo07@10.20.0.241>
2015-07-28 21:44:31 -0500

    Update Producto @daton

--- Maven-Spring-Boot/src/main/java/org/unitec/maven/ControladorProducto.java
+++ Maven-Spring-Boot/src/main/java/org/unitec/maven/ControladorProducto.java
@@ -14,16 +14,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/")
 public class ControladorProducto {
     
-    @RequestMapping(value="/producto/{nombre}/{costo}/{unidades}", method=RequestMethod.POST, headers={"Accept=text/html"})
-    @ResponseBody String guardar(@PathVariable String nombre,@PathVariable Float costo, @PathVariable Integer unidades)throws Exception{
+    @RequestMapping(value="/producto/{id_producto}/{nombre}/{costo}/{unidades}", method=RequestMethod.POST, headers={"Accept=text/html"})
+    @ResponseBody String guardar(@PathVariable Integer id_producto,@PathVariable String nombre,@PathVariable Float costo, @PathVariable Integer unidades)throws Exception{
+        DAOProducto dao=new DAOProducto();
         Producto p=new Producto();
-        p.setCosto(costo);
+        p.setIdProducto(id_producto);
         p.setNombre(nombre);
-        p.setUnidades(unidades);
-        DAOProducto.guardar(p);
+        p.setCosto(costo);
+        p.setUnidades(unidades);        
+        dao.guardar(p);
         
-        return "El producto se ha guardado con exito";
-        
+        return "El producto se ha guardado con exito";        
     }
     
+    @RequestMapping(value="/producto/{id_producto}/{nombre}/{costo}/{unidades}", method=RequestMethod.PUT, headers={"Accept=text/html"})
+    @ResponseBody String actualizar(@PathVariable Integer id_producto,@PathVariable String nombre,@PathVariable Float costo, @PathVariable Integer unidades)throws Exception{
+        DAOProducto dao=new DAOProducto();
+        Producto p=new Producto();
+        p.setIdProducto(id_producto);
+        p.setNombre(nombre);
+        p.setCosto(costo);
+        p.setUnidades(unidades);
+        dao.actualizar(p);
+        return "El producto se ha actualizado con exito";
+    }
 }
--- Maven-Spring-Boot/src/main/java/org/unitec/maven/DAOProducto.java
+++ Maven-Spring-Boot/src/main/java/org/unitec/maven/DAOProducto.java
@@ -1,20 +1,61 @@
 package org.unitec.maven;
 
+import java.util.ArrayList;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 /*
  * @author gatuzo07
  */
 public class DAOProducto {
-    //
-    public static void guardar(Producto p) throws Exception{
+    private SessionFactory fac;
+    private Session ses;
+    private Transaction tranza;
+    
+    public DAOProducto(){
+        fac=HibernateUtilidades.getSessionFactory();
+        ses=fac.openSession();
+        tranza=ses.beginTransaction();
+    }
+    
+    public void cerraSession(){
+        tranza.commit();
+        ses.close();
+    }
+    
+    public void guardar(Producto g)throws Exception{
+        ses.save(g);
+        cerraSession();
+    }
+    
+    public ArrayList<Producto>buscarTodos()throws Exception{
+        Criteria cri=ses.createCriteria(Producto.class);
+        ArrayList<Producto>gastos=(ArrayList<Producto>)cri.list();
+        cerraSession();
+        return gastos;
+    }
+    
+    public Producto buscarPorId(Integer id)throws Exception{
+        Criteria cri=ses.createCriteria(Producto.class);
+        Producto gastos=(Producto)cri.add(Restrictions.idEq(id)).uniqueResult();
+        cerraSession();
+        return gastos;
+    }
+    
+    public void actualizar(Producto g)throws Exception{
+        ses.update(g);
+        cerraSession();
+    }
+}
+/*  public static void guardar(Producto p) throws Exception{
         SessionFactory fac=HibernateUtilidades.getSessionFactory();
         Session ses=fac.openSession();
         Transaction trans=ses.beginTransaction();
         
         ses.save(p);
         ses.close();
-    }
-}
+*/    
+        
